67929cecae3400fd3fdad375b0cbccd2b6568cb5
Donthu Avinash Babu <avinash4216@gmail.com>
2025-11-25 11:17:23 +0530

    feat: [RCL-10] RestTemplate GET API test cases

---
+++ rest-template/src/test/java/com/rcl/resttemplate/GetAPITest.java
@@ -0,0 +1,112 @@
+package com.rcl.resttemplate;
+
+import com.rcl.core.RestClient;
+import com.rcl.core.model.HttpRequest;
+import com.rcl.core.model.HttpResponse;
+import com.rcl.resttemplate.service.RestClientFactoryImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+@Slf4j
+public class GetAPITest {
+
+    @Test
+    public void getApiWithHeaders() {
+        String url = "http://localhost:9000/get/api/v4/students";
+
+        RestClient restClient = RestClientFactoryImpl.getInstance().getClient();
+        log.info("RestClient: " + restClient.getClass().getName());
+
+        Map<String, String> headers = Map.of(
+                "name", "name-1",
+                "book", "book-1"
+        );
+
+        HttpRequest httpRequest = HttpRequest.builder().url(url)
+                .headers(headers)
+                .build();
+        HttpResponse httpResponse = restClient.get(httpRequest);
+        log.info("Response: " + httpResponse.getBody());
+        log.info("Status Code: " + httpResponse.getStatusCode());
+        log.info("Multi map headers: " + httpResponse.getMultiMapHeaders());
+        log.info("Headers: " + httpResponse.getHeaders());
+        Object object = httpResponse.getObject();
+        System.out.println(object);
+    }
+
+    @Test
+    public void getAPIWithRequestParams() {
+        String url = "http://localhost:9000/get/api/v5/students";
+
+        RestClient restClient = RestClientFactoryImpl.getInstance().getClient();
+        log.info("RestClient: " + restClient.getClass().getName());
+
+        Map<String, String> queryParams = Map.of(
+                "name", "name-1",
+                "book", "book-1"
+        );
+
+        HttpRequest httpRequest = HttpRequest.builder().url(url)
+                .queryParams(queryParams)
+                .build();
+        HttpResponse httpResponse = restClient.get(httpRequest);
+        log.info("Response: " + httpResponse.getBody());
+        log.info("Status Code: " + httpResponse.getStatusCode());
+        log.info("Multi map headers: " + httpResponse.getMultiMapHeaders());
+        log.info("Headers: " + httpResponse.getHeaders());
+        Object object = httpResponse.getObject();
+        System.out.println(object);
+    }
+
+    @Test
+    public void getAPIWithPathVariables() {
+        String url = "http://localhost:9000/get/api/v6/students/{name}/{book}";
+
+        RestClient restClient = RestClientFactoryImpl.getInstance().getClient();
+        log.info("RestClient: " + restClient.getClass().getName());
+
+        Map<String, String> pathVariables = Map.of(
+                "name", "name-1",
+                "book", "book-1"
+        );
+
+        HttpRequest httpRequest = HttpRequest.builder().url(url)
+                .pathVariables(pathVariables)
+                .build();
+        HttpResponse httpResponse = restClient.get(httpRequest);
+        log.info("Response: " + httpResponse.getBody());
+        log.info("Status Code: " + httpResponse.getStatusCode());
+        log.info("Multi map headers: " + httpResponse.getMultiMapHeaders());
+        log.info("Headers: " + httpResponse.getHeaders());
+        Object object = httpResponse.getObject();
+        System.out.println(object);
+    }
+
+    @Test
+    public void getAPIWithPaginationRequestParams() {
+        String url = "http://localhost:9000/get/api/v7/students";
+
+        RestClient restClient = RestClientFactoryImpl.getInstance().getClient();
+        log.info("RestClient: " + restClient.getClass().getName());
+
+        Map<String, String> queryParams = Map.of(
+                "page", "0",
+                "size", "1",
+                "sort", "asc"
+        );
+
+        HttpRequest httpRequest = HttpRequest.builder().url(url)
+                .queryParams(queryParams)
+                .build();
+        HttpResponse httpResponse = restClient.get(httpRequest);
+        log.info("Response: " + httpResponse.getBody());
+        log.info("Status Code: " + httpResponse.getStatusCode());
+        log.info("Multi map headers: " + httpResponse.getMultiMapHeaders());
+        log.info("Headers: " + httpResponse.getHeaders());
+        Object object = httpResponse.getObject();
+        System.out.println(object);
+    }
+
+}
